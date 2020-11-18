9f5abd7e83ecf81bc20ed7d87e01a3718c64e2cf
Aidar4ik <flayjk@mail.ru>
2020-11-18 20:10:57 +0600

    Initial commit

---
+++ src/com/company/Cars.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Cars {
+    private Color color;
+    Fuel fuel = new Fuel();
+
+    public Cars(Color color, Fuel fuel) {
+        this.color = color;
+        this.fuel = fuel;
+    }
+
+    public Cars() {
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+
+    public Fuel getFuel() {
+        return fuel;
+    }
+
+    final String getInfo() {
+        return "Цвет:" + color + "|Топливо:" + fuel.getFuelName() + "|Расход:" + fuel.getFuelConsumption();
+    }
+
+}
